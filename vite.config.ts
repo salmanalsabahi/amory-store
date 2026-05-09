2a6a2d873326b14975702cfaf81a764b521021f1
سلمان احمد علي الصباحي <115709825+salmanalsabahi@users.noreply.github.com>
2026-05-09 10:32:10 +0300

    feat(pwa): Increase maximum file size for PWA caching
    Increases the maximum file size that can be cached by the Workbox service worker from the default to 5MB. This will allow larger assets, such as high-resolution images, to be cached for offline access, improving the performance and reliability of the عموري ستور PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,9 @@ export default defineConfig(({mode}) => {
               purpose: 'any maskable'
             }
           ]
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5000000 // 5 MB
         }
       })
     ],
